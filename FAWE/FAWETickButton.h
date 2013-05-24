9a65c4d0658412d9da0b33cacce6ed47fc6cce2e
Juha Suni <juha.suni@mediacabinet.fi>
2013-05-24 14:41:16 +0300

    Adds new button FAWETickButton

---
+++ FAWE/FAWETickButton.h
@@ -0,0 +1,21 @@
+//
+//  ShowellTickButton.h
+//  FAWE
+//
+//  Created by Juha Suni on 24.5.2013.
+//  Copyright (c) 2013 Media Cabinet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "NSString+FontAwesome.h"
+#import <QuartzCore/QuartzCore.h>
+
+@interface FAWETickButton : UIButton
+
+@property (nonatomic, weak) CAShapeLayer *circleLayer;
+@property (nonatomic, strong) UIColor *circleColor;
+
+- (void)setIcon:(FAWEIcon)icon forState:(UIControlState)state;
+
+- (void)setIconColor:(UIColor *)color forState:(UIControlState)state;
+@end
---
+++ FAWE/FAWETickButton.m
@@ -0,0 +1,59 @@
+//
+//  ShowellTickButton.m
+//  FAWE
+//
+//  Created by Juha Suni on 24.5.2013.
+//  Copyright (c) 2013 Media Cabinet. All rights reserved.
+//
+
+#import "FAWETickButton.h"
+#import <QuartzCore/QuartzCore.h>
+#import "NSString+FontAwesome.h"
+
+@implementation FAWETickButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    
+    if (self) {
+        
+        self.circleColor = [UIColor blackColor];
+        
+        self.circleLayer = [CAShapeLayer layer];
+        self.circleLayer.zPosition = -1;
+        
+        float radius = self.frame.size.width / 2;
+        self.circleLayer.path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(0, 0, 2.0*radius, 2.0*radius)
+                                                 cornerRadius:radius].CGPath;
+        self.circleLayer.position = CGPointMake(0, 0);
+        
+        [self.layer addSublayer:self.circleLayer];
+    }
+    
+    return self;
+}
+
+- (void)layoutSubviews
+{
+    self.titleLabel.frame = self.frame;
+    self.titleLabel.font = [UIFont fontWithName:kFontAwesomeFamilyName size:self.frame.size.width/2];
+    
+    [super layoutSubviews];
+}
+
+- (void)setIcon:(FAWEIcon)icon forState:(UIControlState)state
+{
+    [super setTitle:[NSString fontAwesomeIconStringForEnum:icon] forState:state];
+}
+
+- (void)setIconColor:(UIColor *)color forState:(UIControlState)state
+{
+    [super setTitleColor:color forState:state];
+}
+
+- (void)drawRect:(CGRect)rect
+{
+   self.circleLayer.fillColor = self.circleColor.CGColor;
+}
+@end
